2ce2c921959e8aef1836a59937f45c1433b1f8eb
bishamon82 <tikflydotio@gmail.com>
2026-03-29 08:02:56 +0700

    Add files via upload

---
+++ scripts/helpers/utils.py
@@ -0,0 +1,11 @@
+def api_score(item, m, C):
+  R = item.get('rating', 0)
+  v = item.get('rating_count', 0)
+  return (v / (v + m)) * R + (m / (v + m)) * C if (v + m) else 0
+
+def sort_api_list(apis):
+  m = 50
+  C = sum(x['rating'] for x in apis) / len(apis)
+  apis.sort(key=lambda x: api_score(x, m, C), reverse=True)
+
+  return apis
